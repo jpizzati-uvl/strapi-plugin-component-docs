39ab77b4bbba459cbf36ab747ab00b5dc01d47a9
Jorge Pizzati <jorge.pizzati@uvltd.tech>
2025-04-10 13:17:43 +0200

    chore: cleanup

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -1,7 +1,6 @@
 import { PLUGIN_ID } from './pluginId';
 import { Initializer } from './components/Initializer';
 import {PluginIcon} from './components/PluginIcon';
-import './styles/global.css'
 
 export default {
   register(app: any) {
